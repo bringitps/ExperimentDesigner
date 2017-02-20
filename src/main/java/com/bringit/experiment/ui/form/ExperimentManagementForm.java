6a9d4c21f4c32799b521cc696f36682ba3ecc4ad
Edgar Beltran <edgar.beltran@bringitps.com>
2017-02-19 21:12:38 -0600

    - Filling Data Table

--- src/main/java/com/bringit/experiment/ui/form/ExperimentManagementForm.java
+++ src/main/java/com/bringit/experiment/ui/form/ExperimentManagementForm.java
@@ -1,40 +1,16 @@
 package com.bringit.experiment.ui.form;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
 
-import com.bringit.experiment.WebApplication;
-import com.bringit.experiment.bll.Experiment;
-import com.bringit.experiment.bll.SysUser;
 import com.bringit.experiment.dao.DataBaseViewDao;
-import com.bringit.experiment.dao.ExperimentDao;
 import com.bringit.experiment.ui.design.ExperimentManagementDesign;
-import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.server.VaadinService;
+import com.bringit.experiment.util.VaadinControls;
 
 public class ExperimentManagementForm extends ExperimentManagementDesign {
 	
-	@SuppressWarnings("deprecation")
 	public ExperimentManagementForm() {
-		tblExperiments.removeAllColumns();
 		ResultSet experimentViewResults = new DataBaseViewDao().getViewResults("vwExperiment");
-		String test = "";
-		try {
-			
-		    while (experimentViewResults.next()) {
-		      test +=experimentViewResults.getString(1) + ", " + experimentViewResults.getString(2) + ", "
-		          + experimentViewResults.getString(3);
-		    }
-		    System.out.println(test);
-			tblExperiments.setCaption("Testing: " + test);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	    //tblExperiments.setContainerDataSource(new BeanItemContainer(Experiment.class, new ExperimentDao().getAllExperiments()));
-	    
+		VaadinControls.bindDbViewRsToVaadinTable(tblExperiments, experimentViewResults, 1);
 	}
 	
 }
---
+++ src/main/java/com/bringit/experiment/util/VaadinControls.java
@@ -0,0 +1,37 @@
+package com.bringit.experiment.util;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import com.vaadin.ui.Table;
+
+public class VaadinControls {
+
+	public static void bindDbViewRsToVaadinTable(Table vaadinTable, ResultSet dbVwResultSet, int columnIdIndex)
+	{
+		vaadinTable.setContainerDataSource(null);
+		vaadinTable.removeAllItems();
+		
+		ResultSetMetaData dbVwMetaData;
+		try {
+				dbVwMetaData = dbVwResultSet.getMetaData();
+			
+				for(int i=0; i<dbVwMetaData.getColumnCount(); i++)
+					vaadinTable.addContainerProperty(dbVwMetaData.getColumnLabel(i+1), String.class, null);
+				 
+			    while (dbVwResultSet.next()) {
+			    	Object[] itemValues = new Object[dbVwMetaData.getColumnCount()];
+			    	for(int i=0; i<dbVwMetaData.getColumnCount(); i++)
+			    		itemValues[i] = dbVwResultSet.getString(i+1);
+			    	
+			    vaadinTable.addItem(itemValues, dbVwResultSet.getString(columnIdIndex));
+			    } 
+		    
+			}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		
+	    }   
+	}
+}

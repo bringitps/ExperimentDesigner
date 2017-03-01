c18ae6171a012b315cfeb3e7ff7803e1a6826d39
serkor <skortright@jawbone.com>
2017-03-01 01:14:36 -0800

    ExperimentParser updated to use the new design for mapping.
    Include parseCSV function using OpenCsv Jar (included in pom.xml)
    
    Include runTest class to perform a test and upload a sample csv file to
    the database.

---
+++ src/main/java/com/bringit/experiment/dao/CsvDataLoadExecutionResultDao.java
@@ -0,0 +1,112 @@
+package com.bringit.experiment.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.bringit.experiment.bll.CsvDataLoadExecutionResult;
+import com.bringit.experiment.dal.HibernateUtil;
+import com.bringit.experiment.util.HibernateXmlConfigSupport;
+
+public class CsvDataLoadExecutionResultDao {
+
+	private String dialectXmlFile = new HibernateXmlConfigSupport().getHibernateDialectXmlConfigFile();
+	
+	public void addCsvDataLoadExecutionResult(CsvDataLoadExecutionResult csvDataLoadExecutionResult) {
+
+        Transaction trns = null;
+        Session session = HibernateUtil.openSession(dialectXmlFile);
+        
+        try {
+            trns = session.beginTransaction();
+            session.save(csvDataLoadExecutionResult);
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (trns != null) {
+                trns.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+    }
+
+    public void deleteCsvDataLoadExecutionResult(int csvDataLoadExecutionResultId) {
+        Transaction trns = null;
+        Session session = HibernateUtil.openSession(dialectXmlFile);
+        try {
+            trns = session.beginTransaction();
+            CsvDataLoadExecutionResult csvDataLoadExecutionResult = (CsvDataLoadExecutionResult)session.load(CsvDataLoadExecutionResult.class, new Integer(csvDataLoadExecutionResultId));
+            session.delete(csvDataLoadExecutionResult);
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (trns != null) {
+                trns.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+    }
+
+    public void updateCsvDataLoadExecutionResult(CsvDataLoadExecutionResult csvDataLoadExecutionResult) {
+        Transaction trns = null;
+        Session session = HibernateUtil.openSession(dialectXmlFile);
+        try {
+            trns = session.beginTransaction();
+            session.update(csvDataLoadExecutionResult);
+            session.getTransaction().commit();
+        } catch (RuntimeException e) {
+            if (trns != null) {
+                trns.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "unused" })
+	public List<CsvDataLoadExecutionResult> getAllCsvDataLoadExecutionResults() {
+        List<CsvDataLoadExecutionResult> csvDataLoadExecutionResults = new ArrayList<CsvDataLoadExecutionResult>();
+        Transaction trns = null;
+        Session session = HibernateUtil.openSession(dialectXmlFile);
+        try {
+            trns = session.beginTransaction();
+            csvDataLoadExecutionResults = session.createQuery("from CsvDataLoadExecutionResult").list();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return csvDataLoadExecutionResults;
+    }
+
+    @SuppressWarnings("unused")
+	public CsvDataLoadExecutionResult getCsvDataLoadExecutionResultById(int loadId) {
+    	CsvDataLoadExecutionResult csvDataLoadExecutionResult = null;
+        Transaction trns = null;
+        Session session = HibernateUtil.openSession(dialectXmlFile);
+        try {
+            trns = session.beginTransaction();
+            String queryString = "from CsvDataLoadExecutionResult where CsvDataLoadExecId = :id";
+            Query query = session.createQuery(queryString);
+            query.setInteger("id", loadId);
+            csvDataLoadExecutionResult = (CsvDataLoadExecutionResult) query.uniqueResult();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+        return csvDataLoadExecutionResult;
+    }
+	
+}

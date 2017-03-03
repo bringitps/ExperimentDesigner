310bc282245cf5d5157d837958e1d2a6d8b46184
Edgar Beltran <edgar.beltran@bringitps.com>
2017-03-03 16:25:58 -0600

    - View vwXmlDataLoadExecutionResult added to UI

--- src/main/java/com/bringit/experiment/ui/form/XmlDataFileLoadForm.java
+++ src/main/java/com/bringit/experiment/ui/form/XmlDataFileLoadForm.java
@@ -2,13 +2,18 @@ package com.bringit.experiment.ui.form;
 
 
 
+import java.sql.ResultSet;
 import java.util.List;
 
 import com.bringit.experiment.bll.Experiment;
 import com.bringit.experiment.bll.XmlTemplate;
+import com.bringit.experiment.dao.DataBaseViewDao;
 import com.bringit.experiment.dao.ExperimentDao;
 import com.bringit.experiment.dao.XmlTemplateDao;
 import com.bringit.experiment.ui.design.XmlDataFileLoadDesign;
+import com.bringit.experiment.util.VaadinControls;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
@@ -18,6 +23,12 @@ public class XmlDataFileLoadForm extends XmlDataFileLoadDesign{
 
 	public XmlDataFileLoadForm() {
 
+		ResultSet vwXmlDataLoadExecResults = new DataBaseViewDao().getViewResults("vwXmlDataLoadExecutionResult");
+		if(vwXmlDataLoadExecResults != null)
+		{
+			VaadinControls.bindDbViewRsToVaadinTable(tblXmlDataFileLoads, vwXmlDataLoadExecResults, 1);
+			VaadinControls.bindDbViewStringFiltersToVaadinComboBox(cbxXmlDataFileLoadsViewFilters, vwXmlDataLoadExecResults);
+		}
 		btnProcessXmlDataFile.addClickListener(new Button.ClickListener() {
 			
 			@Override
@@ -25,7 +36,13 @@ public class XmlDataFileLoadForm extends XmlDataFileLoadDesign{
 				openProcessXmlDataFileModalWindow();
 			}
 		});
-		
+
+		txtSearch.addShortcutListener(new ShortcutListener("Enter Shortcut", ShortcutAction.KeyCode.ENTER, null) {
+			@Override
+			public void handleAction(Object sender, Object target) {
+				filterDbViewResults();
+			}
+			});
 	}
 	
 	public void openProcessXmlDataFileModalWindow()
@@ -48,5 +65,16 @@ public class XmlDataFileLoadForm extends XmlDataFileLoadDesign{
 		 this.getUI().addWindow(processXmlDataFileModalWindow);
     }
 	
+
+	private void filterDbViewResults()
+	{
+		if(cbxXmlDataFileLoadsViewFilters.getValue() != null)
+		{
+			ResultSet experimentViewResults = new DataBaseViewDao().getFilteredViewResults("vwXmlTemplate", (String)cbxXmlDataFileLoadsViewFilters.getValue(), txtSearch.getValue());
+
+			if(experimentViewResults != null)
+				VaadinControls.bindDbViewRsToVaadinTable(tblXmlDataFileLoads, experimentViewResults, 1);
+		}
+	}
 	
 }
